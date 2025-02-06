9eb8f1d5adead757eeab2f60f41cf6f422433170
Usama <usama345327@gmail.com>
2025-02-06 23:21:36 +0500

    Adding custom instrumentions in python app to generate metrics for prometheus

---
+++ prom_python_app/gauge.py
@@ -0,0 +1,28 @@
+import http.server
+import random
+import time
+from prometheus_client import start_http_server, Gauge
+
+REQUEST_IN_PROGRESS = Gauge('app_requests_inprogress','number of application requests in progress')
+REQUEST_LAST_SERVED = Gauge('app_request_last_served', 'Time the application was last served')
+APP_PORT = 8000
+METRICS_PORT = 8001
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+    @REQUEST_IN_PROGRESS.track_inprogress()
+    def do_GET(self):
+        # REQUEST_IN_PROGRESS.inc()
+        time.sleep(5)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>First Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Prometheus-Python application.</center></h2></body></html>", "utf-8"))
+        self.wfile.close()
+        # REQUEST_IN_PROGRESS.dec()
+        # REQUEST_LAST_SERVED.set(time.time())
+        REQUEST_LAST_SERVED.set_to_current_time()
+
+if __name__ == "__main__":
+    start_http_server(METRICS_PORT)
+    server = http.server.HTTPServer(('localhost', APP_PORT), HandleRequests)
+    server.serve_forever()
